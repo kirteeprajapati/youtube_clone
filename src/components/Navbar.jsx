46fd911e2f84f9feff334735aba27637d5ec922a
Kirtee Prajapati <95649744+kirteeprajapati@users.noreply.github.com>
2023-09-03 15:34:41 +0530

    Added comments to navbar.js

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 import { logo } from "../utils/constants";
 import { SearchBar } from "./";
-
+/* Stack is mui components 1D layout in columns or rows*/
 const Navbar = () => (
   <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
     <Link to="/" style={{ display: "flex", alignItems: "center" }}>
@@ -14,3 +14,5 @@ const Navbar = () => (
 );
 
 export default Navbar;
+
+/*sx is used to add style to mui component */
